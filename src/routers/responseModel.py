d355b4aed37f36ac7970ed58ec01554b71e78f6d
dengke <dengke@engine3d.com>
2026-09-11 15:20:56 +0800

    增加高级教程的直接返回响应代码和测试

--- src/routers/responseModel.py
+++ src/routers/responseModel.py
@@ -2,12 +2,18 @@ from fastapi import APIRouter, Response
 from typing import Any
 from pydantic import BaseModel
 from fastapi.responses import JSONResponse, RedirectResponse
+from fastapi.encoders import jsonable_encoder
 
 router = APIRouter()
 
 class Item(BaseModel):
     name: str
     tags: list[str] = []
+
+# 高级-直接返回响应
+class ItemResponse(BaseModel):
+    name: str
+    desc: str | None = None
 
 @router.post("/items/", response_model=Item)
 async def create_item(item: Item) -> Any:
@@ -29,3 +35,9 @@ async def get_portal(teleport: bool = False) -> Response:
         return RedirectResponse(url="https://www.youtube.com")
     return JSONResponse(content={"message": "Here your interdimensional portal"})
 
+
+# 高级教程 直接返回响应
+@router.put("/itemresponse/{id}")
+async def update_item_response(id: str, item: ItemResponse):
+    json_compatible_item_data = jsonable_encoder(item)
+    return JSONResponse(content=json_compatible_item_data)
